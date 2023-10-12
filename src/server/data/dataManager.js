b90dbfdfa965a27d608e2df100a63400ba94e9f3
CodeSandbox <support@codesandbox.io>
2023-10-12 08:02:24 +0000

    Sandbox snapshot commit

--- src/server/data/dataManager.js
+++ src/server/data/dataManager.js
@@ -70,8 +70,7 @@ const collection = function ({
     self.set(...self.get(), ...items);
     return items.map((x) => self.getKey(x));
   };
-  self.remove = (item) =>
-    self.set(...self.get().filter((x) => self.getKey(x) !== item.getKey(x)));
+  self.remove = (item) => self.set(...self.get().filter((x) => x !== item));
   self.filterBy = (filter) =>
     self
       .get()
